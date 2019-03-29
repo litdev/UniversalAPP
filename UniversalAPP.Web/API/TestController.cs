8744f86bfdd76cdd81511dfbc396cf05cfe992e2
Yang <litdev@outlook.com>
2019-03-29 18:08:09 +0800

    添加Swagger

--- UniversalAPP.Web/API/TestController.cs
+++ UniversalAPP.Web/API/TestController.cs
@@ -10,10 +10,17 @@ using Microsoft.Extensions.Options;
 
 namespace UniversalAPP.Web.API
 {
+    /// <summary>
+    /// 测试控制器
+    /// </summary>
     [Produces("application/json")]
     [Route("api/Test")]
     public class TestController : BaseAPIController
     {
+        /// <summary>
+        /// 测试接口是否通
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Route("doing")]
         public UnifiedResultEntity<string> doing()
@@ -21,5 +28,22 @@ namespace UniversalAPP.Web.API
             //throw new Exception("sss");
             return ResultBasicString(1, "OK", "接口已通");
         }
+
+        /// <summary>
+        /// 带参数的接口
+        /// </summary>
+        /// <param name="val">随便写点啥</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("doing/other")]
+        public UnifiedResultEntity<int> doing2(string val)
+        {
+            UnifiedResultEntity<int> resultEntity = new UnifiedResultEntity<int>();
+            resultEntity.msg = 1;
+            resultEntity.msgbox = $"接口已通:{val}";
+            resultEntity.data = 222;
+            return resultEntity;
+        }
+
     }
 }
--- UniversalAPP.Web/Startup.cs
+++ UniversalAPP.Web/Startup.cs
@@ -12,40 +12,58 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Hangfire;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace UniversalAPP.Web
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public IConfiguration Configuration { get; }
 
 
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
+            #region 初始化公共DI需要
             services.AddHttpContextAccessor();
+            #endregion
 
-            ///Cookie GDPR政策
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => false; //设置为False为禁用这个规范，问题：https://blog.csdn.net/shiershilian/article/details/80876803
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            #region 配置文件
 
             //注入配置文件
             services.Configure<Models.SiteBasicConfig>(Configuration.GetSection("SiteBasicConfig"));
             //用户机密
             //var secrets_val = Configuration.GetSection("Email:Pwd").Value;
 
+            #endregion
+
+            #region 数据库
+
             services.AddDbContext<EFCore.EFDBContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), p => p.UseRowNumberForPaging()));
+
+            #endregion
+
+            #region 身份验证
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(option =>
             {
@@ -67,6 +85,10 @@ namespace UniversalAPP.Web
 
             #endregion
 
+            #endregion
+
+            #region MVC
+            
             //添加MVC及过滤器
             services.AddMvc(options =>
             {
@@ -78,15 +100,56 @@ namespace UniversalAPP.Web
             }).SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
             services.AddSession();
 
+            #endregion
+
+            #region Cookie GDPR
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => false; //设置为False为禁用这个规范，问题：https://blog.csdn.net/shiershilian/article/details/80876803
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            #endregion
+
+            #region Hangfire计划任务
+
             ////Hangfire计划任务
             //services.AddHangfire(p => p.UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection")));
 
+
+            #endregion
+
+            #region Swagger API文档生成
+            ////注册Swagger生成器，定义一个和多个Swagger 文档
+            ////属性>生成>XML文档文件 bin\debug\netcoreapp2.2\Swagger.xml
+            ////属性>生成>禁止显示警告 1701;1702
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new Info { Title = "UniversalAPP Web API Document", Version = "v1" });
+            //    // 为 Swagger JSON and UI设置xml文档注释路径
+            //    var basePath = System.IO.Path.GetDirectoryName(typeof(Program).Assembly.Location);//获取应用程序所在目录（绝对，不受工作目录影响，建议采用此方法获取路径）
+            //    var xmlPath = System.IO.Path.Combine(basePath, "Swagger.xml");
+            //    c.IncludeXmlComments(xmlPath);
+            //});
+
+            #endregion
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            #region 初始化公共DI需要
             ServiceLocator.Instance = app.ApplicationServices;
+            #endregion
+
+            #region 判断运行环境
 
             if (env.IsDevelopment())
             {
@@ -104,23 +167,18 @@ namespace UniversalAPP.Web
                 app.UseHsts();
             }
 
+            #endregion
+            
+            #region MVC相关
+
             app.UseHttpsRedirection();
-
-            //验证
             app.UseAuthentication();
-            //图片不存在时返回默认图片
-            app.UseDefaultImage(defaultImagePath: Configuration.GetSection("defaultImagePath").Value);
-
-            System.Diagnostics.Trace.Listeners.Clear();
-            System.Diagnostics.Trace.Listeners.Add(new CustomTraceListener());
-
             //获取IP
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 //为了在nginx上获取用户端真实IP添加此代码
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
-
             //app.UseCookiePolicy();//增加对于GDPR政策的支持
             app.UseSession();
             app.UseStaticFiles();
@@ -133,10 +191,38 @@ namespace UniversalAPP.Web
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            #endregion
+
+            #region 其他公共
+
+            //图片不存在时返回默认图片
+            app.UseDefaultImage(defaultImagePath: Configuration.GetSection("defaultImagePath").Value);
+
+            System.Diagnostics.Trace.Listeners.Clear();
+            System.Diagnostics.Trace.Listeners.Add(new CustomTraceListener());
+
+            #endregion
+
+            #region Hangfire计划任务
+
             ////Hangfire计划任务
             //app.UseHangfireServer();
             //app.UseHangfireDashboard("/hangfire", new DashboardOptions { Authorization = new[] { new HangfireAuthorizationFilter() } });
 
+            #endregion
+
+            #region Swagger API文档生成
+
+            ////启用中间件服务生成Swagger作为JSON终结点
+            //app.UseSwagger();
+            ////启用中间件服务对swagger-ui，指定Swagger JSON终结点
+            //app.UseSwaggerUI(c =>
+            //{
+            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UniversalAPP Web API Document");
+            //});
+
+            #endregion
         }
     }
 }
